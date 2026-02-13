8294e7f2611c74e1645d5caa7d955802a54b99ac
Anabelly Passos <anabellybel123@gmail.com>
2026-02-13 15:32:48 -0300

    criação de pipe para telefone

--- src/app/pipes/phone.pipe.ts
+++ src/app/pipes/phone.pipe.ts
@@ -6,10 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhonePipe implements PipeTransform {
 
   transform(phone: string): string {
-    if (!phone || phone.length < 10 || phone.length > 11){
-      return'Telefone inválido'
+    const InVALID_PHONE = !phone || phone.length < 10 || phone.length > 11;
+    if (InVALID_PHONE) {
+      return 'Telefone indisponivel ou inválido'
     }
+    const CELLPHONE = phone.length === 11;
+    if (CELLPHONE) {
+      return `(${phone.substring(0,2)})  ${phone.substring(2,7)}-${phone.substring(7)} `;
 
+    } else {
+      return `(${phone.substring(0,2)})  ${phone.substring(2,6)}-${phone.substring(6)} `;
+    }
   }
 
 }
