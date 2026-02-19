a6e8016d7ff101669510ba4b26f294c4f8f48bf5
Anabelly Passos <anabellybel123@gmail.com>
2026-02-19 18:42:36 -0300

    pipe para valores vazios

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
 import { StatusPipe } from './status.pipe';
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 
 
@@ -11,12 +12,14 @@ import { StatusPipe } from './status.pipe';
 declarations:[
     PhonePipe,
     AddressPipe,
-    StatusPipe
+    StatusPipe,
+    DashIfEmptyPipe
   ],
 exports:[
   PhonePipe,
   AddressPipe,
-  StatusPipe
+  StatusPipe,
+  DashIfEmptyPipe
 ],
 
 })
