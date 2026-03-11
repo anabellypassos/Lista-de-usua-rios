367a4256442c77029849714cc391c0cb36795ba1
Anabelly Passos <anabellybel123@gmail.com>
2026-03-11 15:25:44 -0300

    filterOptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -22,4 +23,8 @@ setTimeout (()=>{
     this.userSelected = user;
     this.showUserDetails = true;
   }
+  onFilter( filterOptions: IFilterOptions){
+    console.log(filterOptions)
+}
+
 }
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IFilterOptions } from 'src/app/interfaces/filter-options.interface';
 
 @Component({
@@ -21,7 +21,8 @@ export class FilterComponent {
       {description:'Inativo', value: false}
 
   ];
+  @Output('onFilter') onFilterEmitt = new  EventEmitter <IFilterOptions>();
   onFilter (){
-    console.log(this.filterOptions);
+    this.onFilterEmitt.emit(this.filterOptions);
   }
 }
